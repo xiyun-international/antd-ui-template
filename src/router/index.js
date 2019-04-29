08c474096c948ca8c86607c99bf702343141096c
zhaoliang <kybetter@163.com>
2019-04-29 11:08:15 +0800

    feat: 增加iframe逻辑

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import GlobalMixin from '@/utils/global-mixin';
+
 import Login from '@/views/login/index.vue';
 import Layout from '@/views/layout/index.vue';
 import children from './children';
@@ -26,6 +28,16 @@ const routes = [
 const Router = new VueRouter({ routes });
 
 Router.beforeEach((to, from, next) => {
+  // 如果项目是简单 layout 时，那么需要向父页面发送 postMessage 来调整子页面 iframe 高度。
+  if (process.env.VUE_APP_MODE === 'simple') {
+    const components = router.getMatchedComponents(to);
+    if (components.length === 1) {
+      components[0].mixins.push(GlobalMixin);
+    } else {
+      components[components.length - 1].mixins.push(GlobalMixin);
+    }
+  }
+
   next();
 });
 
