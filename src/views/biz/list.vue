6351918b5fd6e5108cac19a68515224ea0168f85
huqiangqiang <huqiangqiang@xiyun.com.cn>
2019-07-30 20:18:40 +0800

    feat: 新增列表页

--- src/views/biz/list.vue
+++ src/views/biz/list.vue
@@ -1,36 +1,104 @@
 <template>
   <xy-context
-    :breadcrumb="[
-      { name: '服务商中心', path: '' },
-      { name: '协作费管理', path: '' },
-      { name: '账单详情' },
-    ]"
-    title="账单详情"
+    :breadcrumb="[{ name: '列表', path: 'biz/list' }, { name: '用户列表', path: 'biz/list' }]"
+    title="用户列表"
     tag="审核中"
     :tag-status="5"
   >
-    <p>我是内容区</p>
-    <p>我是内容区</p>
-    <p>我是内容区</p>
-    <p>我是内容区</p>
-    <p>我是内容区</p>
-    <p>我是内容区</p>
-    <p>我是内容区</p>
-    <p>我是内容区</p>
-    <p>我是内容区</p>
-    <p>我是内容区</p>
-    <p>我是内容区</p>
-    <p>我是内容区</p>
-    <p>我是内容区</p>
     <div slot="right">
       <a-button type="primary">打印账单</a-button>
     </div>
+
+    <a-row>
+      <a-col>
+        <search-form @onSearch="onSearch"></search-form>
+      </a-col>
+    </a-row>
+
+    <a-row class="t-MT30">
+      <a-table
+        :rowKey="record => record.id"
+        :columns="columns"
+        :dataSource="List"
+        :pagination="true"
+      >
+        <template slot="action" slot-scope="_, record">
+          <router-link :to="`/supplier/detail/${record.id}`">
+            查看
+          </router-link>
+        </template>
+      </a-table>
+    </a-row>
   </xy-context>
 </template>
 
 <script>
+import SearchForm from './form/search.vue';
+
 export default {
   name: 'listDemo',
+  data() {
+    return {
+      searchForm: {},
+      pagination: {
+        page: 1,
+        pageSize: 10,
+        totalCount: 0,
+      },
+      columns: [
+        {
+          title: '单号',
+          dataIndex: 'code',
+          width: '30%',
+        },
+        {
+          title: '时间',
+          dataIndex: 'time',
+        },
+        {
+          title: '状态',
+          dataIndex: 'status',
+        },
+        {
+          title: '操作',
+          dataIndex: 'action',
+          scopedSlots: { customRender: 'action' },
+        },
+      ],
+      List: [
+        {
+          id: 1,
+          code: 1111,
+          time: '2019.7.30',
+          status: '没状态',
+        },
+      ],
+    };
+  },
+  components: {
+    SearchForm,
+  },
+  methods: {
+    /**
+     *  获取列表页数据
+     */
+    getList(params = {}) {
+      this.post('/list', params).then(() => {
+        this.$message({
+          type: 'success',
+        });
+      });
+    },
+
+    onSearch(form) {
+      this.searchForm = form;
+      this.getList({
+        ...this.searchForm,
+        page: 1,
+        pageSize: 10,
+      });
+    },
+  },
 };
 </script>
 
