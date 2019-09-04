17f0f7681345f39ca07180b87f69cfb77be8c6f9
wubaiqing (琉璃) <wubaiqing@vip.qq.com>
2019-09-04 18:00:32 +0800

    feat: 优化联调模式

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,10 +6,8 @@ echo $serviceUrl
 sysOS=`uname -s`
 if [ $sysOS == "Darwin" ];then
   sed -i '' "s|VUE_APP_API=.*|VUE_APP_API=${serviceUrl}|" .env;
-  sed -i '' "s|VUE_APP_zhiyunshanUrl=.*|VUE_APP_zhiyunshanUrl=${zhiyunshanUrl}|" .env;
   echo '设置完成'
 else
   sed -i "s|VUE_APP_API=.*|VUE_APP_API=${serviceUrl}|" .env;
-  sed -i "s|VUE_APP_zhiyunshanUrl=.*|VUE_APP_zhiyunshanUrl=${zhiyunshanUrl}|" .env;
   echo '设置完成'
 fi
